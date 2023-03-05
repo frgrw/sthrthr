1d89e988b3d500f122cad9edab2f0ba2be889948
Christopher Berner <christopherberner@gmail.com>
2023-03-04 19:07:28 -0800

    Cleanup config options

--- keyboards/handwired/sculpt/rules.mk
+++ keyboards/handwired/sculpt/rules.mk
@@ -8,10 +8,10 @@ BOOTLOADER = atmel-dfu
 # Interrupt driven control endpoint task(+60)
 OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
+BOOTMAGIC_ENABLE = yes       # Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE = yes        # Console for debug(+400)
+CONSOLE_ENABLE = no        # Console for debug(+400)
 COMMAND_ENABLE = no        # Commands for debug and configuration
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no            # USB Nkey Rollover
@@ -20,6 +20,6 @@ MIDI_ENABLE = no            # MIDI controls
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
-DIP_SWITCH_ENABLE = yes
+DIP_SWITCH_ENABLE = yes     # Virtual DIP switch configuration(+1000)
 
 SRC += matrix.c
